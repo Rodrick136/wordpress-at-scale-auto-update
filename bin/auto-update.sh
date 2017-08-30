b438aecfc2eacf9904072a1058a6c09c6a1262ce
Rodrick136 <rodrickdusio@gmail.com>
2017-08-30 16:18:43 -0700

    Revisions to support json manipulation

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 UPDATES_APPLIED=false
+reference_Url="https://${MULTIDEV}-${SITENAME}.pantheonsite.io/"
+reference_Url_contact="${reference_Url}/contact-us/"
 
 # Stash Circle Artifacts URL
 CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
@@ -138,14 +140,19 @@ else
 
     # backstop visual regression
     echo -e "\nRunning BackstopJS tests..."
+	
+	# Modify Backstop.json for the site reference
+	echo -e "\nUpdating Multi Dev URL..."
+	MULTIDEV_URL_UPDATES=$(bash ./$SITENAME/manipulate_json.sh)
+	
 
     # Backstop visual regression
     echo -e "\nRunning backstop reference..."
 
-    backstop reference --config=$VARIANT_FILE
+    backstop reference --config=$VARIANT_FILE/backstop.json
 
     echo -e "\nRunning backstop test..."
-    VISUAL_REGRESSION_RESULTS=$(backstop test --config=${VARIANT_FILE} || echo 'true')
+    VISUAL_REGRESSION_RESULTS=$(backstop test --config=${VARIANT_FILE}/backstop.json || echo 'true')
 
     echo "${VISUAL_REGRESSION_RESULTS}"
 
