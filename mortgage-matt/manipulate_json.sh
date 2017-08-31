72ee5d1b927e937efb39ac67d2bff6a0602c1de8
Rodrick136 <rodrickdusio@gmail.com>
2017-08-30 17:09:19 -0700

    Added Morgate Matt site folder

---
+++ mortgage-matt/manipulate_json.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+VARIANT_FILE="./c-cornerstone"
+
+reference_Url="https://${MULTIDEV}-${SITENAME}.pantheonsite.io/"
+reference_Url_contact="${reference_Url}/contact-us/"
+
+jq --arg reference_Url $reference_Url --arg reference_Url_contact $reference_Url_contact 'del(.scenarios | .? | .[0] | .referenceUrl) |(.scenarios[] | select(.label == "Homepage")) |= . + {"referenceUrl": $reference_Url} |del(.scenarios | .? | .[1] | .referenceUrl) |(.scenarios[] | select(.label == "Contact Us")) |= . + {"referenceUrl": $reference_Url_contact }' $VARIANT_FILE/backstop.json > $VARIANT_FILE/tmp.$$.json
+
+mv $VARIANT_FILE/tmp.$$.json $VARIANT_FILE/backstop.json
